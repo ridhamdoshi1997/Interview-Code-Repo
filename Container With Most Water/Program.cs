f780af25488e43f6885813d09f316eb9bfef5264
Ridham Doshi <v-rdoshi@microsoft.com>
2022-04-26 17:58:10 -0400

    DFS

---
+++ Container With Most Water/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Container_With_Most_Water
+{
+    internal class Program
+    {
+        public static int MaxArea(int[] height)
+        {
+            if(height.Length<=1)
+            {
+                return 0;
+            }
+            int Max_Area = 0;
+
+            /* Brute Force 
+            for (int i = 0; i < height.Length; i++)
+            {
+                for (int j = i + 1; j < height.Length; j++)
+                {
+                    int New_Max_Area = Math.Min(height[i], height[j]) * (j - i);
+                    if (New_Max_Area > Max_Area)
+                    {
+                        Max_Area = New_Max_Area;
+                    }
+                }
+            }
+
+            */
+            int a = 0;
+            int b = height.Length-1;
+            while(a<b)
+            {
+                int New_Max_Area = Math.Min(height[a], height[b]) * (b - a);
+
+                if(New_Max_Area > Max_Area)
+                {
+                   Max_Area = New_Max_Area;
+                }
+                if(height[a]<height[b])
+                {
+                    a++;
+                }
+                else
+                {
+                    b--;
+                }
+            }
+            return Max_Area;    
+        }
+        static void Main(string[] args)
+        {
+            int vs = MaxArea(new int[] { 2, 7, 11, 15 });
+            
+           Console.WriteLine(vs); ;
+        }
+    }
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwoSum
+{
+    internal class Program
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+
+            int len = nums.Length;
+            Dictionary<int, int> visited = new Dictionary<int, int>();
+            for (int i = 0; i < len; i++)
+            {
+                var lookingFor = target - nums[i];
+                if (visited.ContainsKey(lookingFor))
+                    return new int[] { visited[lookingFor], i };
+                else
+                    if (!visited.ContainsKey(nums[i]))
+                    visited.Add(nums[i], i);
+            }
+            return new int[0];
+        }
+        static void Main(string[] args)
+        {
+            int[] vs = TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            foreach (int i in vs)
+                Console.WriteLine(vs[i]);
+        }
+    }
+}
